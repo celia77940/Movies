f08bf26fc6d20d21dfc5f22ac7f6c170ec279aed
celia77940 <celiarameau@live.fr>
2021-07-27 18:13:19 +0200

    scss

---
+++ src/components/utils/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return(
+        <div style={{minHeight: '100vh'}} className="d-flex flex-row justify-content-center align-items-center w-100">
+           <img alt='loadind gif' src='https://i.pinimg.com/originals/a2/dc/96/a2dc9668f2cf170fe3efeb263128b0e7.gif'/>
+        </div>
+    );
+}
