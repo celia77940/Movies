adcad4ba3d36dddc7f6060278dcf489f1c2d6ef0
celia77940 <celiarameau@live.fr>
2021-08-15 13:27:41 +0200

    api et barre de recherche

--- src/components/movie-details/MovieDetails.js
+++ src/components/movie-details/MovieDetails.js
@@ -9,7 +9,7 @@ export default class MovieDetails extends Component {
         <div>
           <img
             alt={this.props.movie.title}
-            className="d-block mx-auto"
+            className="d-block mx-auto w-100"
             src={this.props.movie.img}
           />
         </div>
---
+++ src/conf/api.movie.js
@@ -0,0 +1,12 @@
+import * as axios from 'axios';
+
+const apiMovie = axios.create({
+    baseURL : 'https://api.themoviedb.org/4'
+})
+
+export default apiMovie;
+
+apiMovie.interceptors.request.use( req => {
+    req.headers['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NGRkMWY0ODQxYzU3MjBjODhiNzNlMmZlMzM2YjM2YyIsInN1YiI6IjYxMTc4MjFiMjU4ODIzMDA0Nzc3ZWY2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0S-qeq04UJIvawij-g8Qfn2ApLZFKiwDqtMSYJMFkjk'
+    return req;
+})
