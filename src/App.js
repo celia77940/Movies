ac68be04073485fe93cc41ae507491a5ed402a69
celia77940 <celiarameau@live.fr>
2021-08-16 10:51:41 +0200

    fini barre de recherche

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Header, MovieList, MovieDetails, Loading, SearchBar } from './components';
-import apiMovie from './conf/api.movie';
+import apiMovie, {apiMovieMap} from './conf/api.movie';
 
 class App extends Component {
 
@@ -23,19 +23,14 @@ class App extends Component {
     apiMovie.get('/discover/movie')
       .then( response => response.data.results)
       .then( moviesApi => {
-        const movies = moviesApi.map( m => ({
-          img: 'https://image.tmdb.org/t/p/w500' + m.poster_path,
-          title: m.title,
-          details: `${ m.release_date } | ${ m.vote_average }/10 (${ m.vote_count })`,
-          description: m.overview
-        }))
+        const movies = moviesApi.map( apiMovieMap )
         this.updateMovies(movies);
       })
       
       .catch( err => console.log(err));
   }
 
-  updateMovies(movies){
+  updateMovies = (movies) => {
     this.setState({
       movies,
       loaded: true
--- src/conf/api.movie.js
+++ src/conf/api.movie.js
@@ -10,3 +10,10 @@ apiMovie.interceptors.request.use( req => {
     req.headers['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NGRkMWY0ODQxYzU3MjBjODhiNzNlMmZlMzM2YjM2YyIsInN1YiI6IjYxMTc4MjFiMjU4ODIzMDA0Nzc3ZWY2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0S-qeq04UJIvawij-g8Qfn2ApLZFKiwDqtMSYJMFkjk'
     return req;
 })
+
+export const apiMovieMap = m => ({
+    img: 'https://image.tmdb.org/t/p/w500' + m.poster_path,
+    title: m.title,
+    details: `${ m.release_date } | ${ m.vote_average }/10 (${ m.vote_count })`,
+    description: m.overview
+  })
